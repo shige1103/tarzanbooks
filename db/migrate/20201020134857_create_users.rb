9d21ba2158303d20b6a78c66e518edae2c121024
shigenori miyazaki <s1i1g0e3@gmail.com>
2020-10-21 04:08:02 +0000

    user signup

---
+++ db/migrate/20201020134857_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :profession
+      t.string :first_name
+      t.string :last_name
+      t.text :profile
+      t.timestamp :icon
+      t.string :new_email
+      t.string :new_password
+
+      t.timestamps
+    end
+  end
+end
